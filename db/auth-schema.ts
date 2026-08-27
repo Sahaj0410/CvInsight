cc81bd718c3efdfb45ae22750e56602af90e013b
Sahaj <sahajpaun01@gmail.com>
2026-08-27 14:35:32 +0530

    Solved login issue by adding a new column to the auth schema and updating the SQL migration script accordingly.

--- db/auth-schema.ts
+++ db/auth-schema.ts
@@ -41,6 +41,7 @@ export const account = pgTable("account", {
     accessTokenExpiresAt: timestamp("access_token_expires_at"),
     refreshTokenExpiresAt: timestamp("refresh_token_expires_at"),
     scope: text("scope"),
+    issuer: text("issuer"),
     password: text("password"),
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at")
